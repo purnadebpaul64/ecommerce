8a89b4cc5d3d5802ba0040d813dacad2e65cd8ec
purnadebpaul64 <purnadebpaul64@gmail.com>
2025-08-03 02:33:28 +0600

    added our policy section

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "../components/Home/Hero";
 import LatestCollection from "../components/Home/LatestCollection";
 import BestSeller from "../components/Home/BestSeller";
+import OurPolicy from "../components/Home/OurPolicy";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Hero></Hero>
       <LatestCollection></LatestCollection>
       <BestSeller></BestSeller>
+      <OurPolicy></OurPolicy>
     </section>
   );
 };
